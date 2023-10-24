8dc277cc74d2e48a0048fde44266624c4b12abd3
Durgesh <durgeshmali5824@gmail.com>
2023-10-24 15:09:31 +0530

    pr_9

--- app/src/main/java/com/example/mad_practical_9_21012021047/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021047/SplashActivity.kt
@@ -1,14 +1,18 @@
 package com.example.mad_practical_9_21012021047
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity(),AnimationListener {
     lateinit var  logoanimation : AnimationDrawable
+    lateinit var twinAnimation: Animation
+    lateinit var img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -16,12 +20,15 @@ class SplashActivity : AppCompatActivity(),AnimationListener {
         var image:ImageView = findViewById(R.id.uvpce_logo)
         image.setBackgroundResource(R.drawable.uvpce_animation_list)
         logoanimation = image.background as AnimationDrawable
+        twinAnimation = AnimationUtils.loadAnimation(this, R.anim.twin_animation)
+        twinAnimation.setAnimationListener(this)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
             logoanimation.start()
+            img.startAnimation(twinAnimation)
         }
         else
         {
@@ -33,6 +40,9 @@ class SplashActivity : AppCompatActivity(),AnimationListener {
     }
 
     override fun onAnimationEnd(p0: Animation?) {
+        Intent(this,MainActivity::class.java).also {
+            startActivity(it)
+        }
     }
 
     override fun onAnimationRepeat(p0: Animation?) {
